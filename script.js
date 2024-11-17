a3d9be3be274ac292578454d0f9a46b4be7e8111
Partha Protim Gogoi <176602154+g-partha@users.noreply.github.com>
2024-11-17 22:58:17 +0530

    Edit index.html, style.css, script.js
    Edit index.html, style.css, script.js to add DOM elements for number
    buttons and add event listeners to the buttons to record the input in
    a variable.

--- script.js
+++ script.js
@@ -10,3 +10,69 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1 / num2;
 }
+
+let firstNumber = 0;
+let operator = " ";
+let secondNumber = 0;
+const numberInputArray = [];
+
+function operate(num1, opr, num2){
+    switch(opr){
+        case "+":
+            return add(num1, num2);
+            break;
+        case "-":
+            return subtract(num1, num2);
+            break;
+        case "*":
+            return multiply(num1, num2);
+            break;
+        case "/":
+            return divide(num1, num2);
+            break;
+    }
+}
+
+const displayArea = document.querySelector("#display-container");
+
+const numberButtonsContainer = document.querySelector("#number-buttons-container");
+const numberButtons = [];
+for(let i = 0; i < 10; i++){
+    numberButtons[i] = document.createElement("div");
+    numberButtons[i].innerHTML = i;
+}
+
+numberButtons[10] = document.createElement("div");
+numberButtons[10].innerHTML = ".";
+
+for(let i = 0; i <= 10; i++){
+    numberButtons[i].classList.add("number-buttons");
+    numberButtonsContainer.appendChild(numberButtons[i]);
+    numberButtons[i].addEventListener("click", () => {
+        if(numberInputArray.length <= 16){
+            if(i == 10){
+                if(!numberInputArray.includes(".")){
+                    if(numberInputArray.length == 0){
+                        numberInputArray.push(0);
+                        numberInputArray.push(".");
+                    }else{
+                        numberInputArray.push(".");
+                    }
+                }    
+            }else{
+                numberInputArray.push(i);
+            }
+        }
+        secondNumber = +(numberInputArray.join(""));
+        displayArea.innerHTML = numberInputArray.join("");
+    });
+
+    numberButtons[i].addEventListener("mouseenter", () => {
+        numberButtons[i].classList.add("hover");
+    })
+    numberButtons[i].addEventListener("mouseleave", () => {
+        numberButtons[i].classList.remove("hover");
+    })
+}
+numberButtonsContainer.insertBefore(numberButtons[0], numberButtons[10]);
+
