9ec02c785eaf0b0a862c8f4e0cc1ba3f705a56dd
Partha Protim Gogoi <176602154+g-partha@users.noreply.github.com>
2024-11-19 23:49:56 +0530

    Add delete and clear all button, improve style
    - Add delete and clear all buttons
    - Apply css felxbox property to the containers.
    - Add mousedown and mouseup effects.

--- script.js
+++ script.js
@@ -39,7 +39,6 @@ function operate(num1, opr, num2){
 const displayArea = document.querySelector("#display-container");
 
 // Number buttons
-
 const numberButtonsContainer = document.querySelector("#number-buttons-container");
 const numberButtons = [];
 for(let i = 0; i < 10; i++){
@@ -54,7 +53,7 @@ for(let i = 0; i <= 10; i++){
     numberButtons[i].classList.add("number-buttons");
     numberButtonsContainer.appendChild(numberButtons[i]);
     numberButtons[i].addEventListener("click", () => {
-        if(numberInputArray.length <= 16){
+        if(numberInputArray.length <= 10){
             if(i == 10){
                 if(!numberInputArray.includes(".")){
                     if(numberInputArray.length == 0){
@@ -79,19 +78,26 @@ for(let i = 0; i <= 10; i++){
     numberButtons[i].addEventListener("mouseleave", () => {
         numberButtons[i].classList.remove("hover");
     })
+    numberButtons[i].addEventListener("mousedown", () => {
+        numberButtons[i].classList.remove("hover");
+    })
+    numberButtons[i].addEventListener("mouseup", () => {
+        numberButtons[i].classList.add("hover");
+    })
 }
 numberButtonsContainer.insertBefore(numberButtons[0], numberButtons[10]);
+numberButtons[0].style.cssText = "flex-grow: 1;";
 
 // Operator buttons
-
 const operatorButtons = document.querySelectorAll(".operator-buttons");
-operatorButtons.forEach((button) => {
-    button.addEventListener("click", () => {
+
+function clickOperatorButton(){
         if(numberInputArray.length != 0){
             if(secondNumber == 0 && operator == "/"){
                 alert("Can't divide by 0!");
             }else{
-                result = operate(firstNumber, operator, secondNumber);
+                let resultBeforeRounding = operate(firstNumber, operator, secondNumber);
+                result = (Math.round(resultBeforeRounding * 100) / 100);
             }
         }
         if(result != 0){
@@ -99,37 +105,104 @@ operatorButtons.forEach((button) => {
             displayArea.textContent = result;
         }
         firstNumber = result;
-        operator = button.textContent;
+        operator = this.textContent;
         numberInputArray.splice(0, numberInputArray.length);
-    });
+}
+
+operatorButtons.forEach((button) => {
+    button.addEventListener("click", clickOperatorButton);
+
     button.addEventListener("mouseenter", () => {
         button.classList.add("hover");
     });
     button.addEventListener("mouseleave", () => {
         button.classList.remove("hover");
     });
+    button.addEventListener("mousedown", () => {
+        button.classList.remove("hover");
+    });
+    button.addEventListener("mouseup", () => {
+        button.classList.add("hover");
+    });
 });
 
+//Equal button
 const equalButton = document.querySelector("#equality-button");
-equalButton.addEventListener("click", () => {
+
+function clickEqualButton(){
     if(operator != "empty" && numberInputArray.length != 0 ){
-        if(secondNumber ==0 && operator == "/"){
-            alert("Can't divide by 0!");
-        }else{
-            result = operate(firstNumber, operator, secondNumber);
-        }
-    }
-        if(result != 0){
-            displayArea.textContent = "";
-            displayArea.textContent = result;
-        }
-        firstNumber = result;
-        operator = "empty";
-        numberInputArray.splice(0, numberInputArray.length);
-});
+                if(secondNumber == 0 && operator == "/"){
+                    alert("Can't divide by 0!");
+                }else{
+                    let resultBeforeRounding = operate(firstNumber, operator, secondNumber);
+                    result = (Math.round(resultBeforeRounding * 100) / 100);
+                }
+            }
+            if(result != 0){
+                displayArea.textContent = "";
+                displayArea.textContent = result;
+            }
+            firstNumber = result;
+            operator = "empty";
+            numberInputArray.splice(0, numberInputArray.length);
+}
+
+equalButton.addEventListener("click", clickEqualButton);
 equalButton.addEventListener("mouseenter", () => {
     equalButton.classList.add("hover");
 });
 equalButton.addEventListener("mouseleave", () => {
     equalButton.classList.remove("hover");
+});
+equalButton.addEventListener("mousedown", () => {
+    equalButton.classList.remove("hover");
+});
+equalButton.addEventListener("mouseup", () => {
+    equalButton.classList.add("hover");
+});
+
+// Delete button
+const deleteButton = document.querySelector("#delete-button");
+deleteButton.addEventListener("click", () => {
+    if(numberInputArray.length != 0){
+        numberInputArray.pop();
+        secondNumber = +(numberInputArray.join(""));
+        displayArea.textContent = "";
+        displayArea.textContent = numberInputArray.join("");
+    }
+});
+deleteButton.addEventListener("mouseenter", () => {
+    deleteButton.classList.add("hover");
+});
+deleteButton.addEventListener("mouseleave", () => {
+    deleteButton.classList.remove("hover");
+});
+deleteButton.addEventListener("mousedown", () => {
+    deleteButton.classList.remove("hover");
+});
+deleteButton.addEventListener("mouseup", () => {
+    deleteButton.classList.add("hover");
+});
+
+// All clear button
+const allClearButton = document.querySelector("#all-clear-button");
+allClearButton.addEventListener("click", () => {
+    firstNumber = 0;
+    secondNumber = 0;
+    operator = "empty";
+    result = 0;
+    numberInputArray.splice(0, numberInputArray.length);
+    displayArea.textContent = "";
+});
+allClearButton.addEventListener("mouseenter", () => {
+    allClearButton.classList.add("hover");
+});
+allClearButton.addEventListener("mouseleave", () => {
+    allClearButton.classList.remove("hover");
+});
+allClearButton.addEventListener("mousedown", () => {
+    allClearButton.classList.remove("hover");
+});
+allClearButton.addEventListener("mouseup", () => {
+    allClearButton.classList.add("hover");
 });
